f4db54f627a4b89e8a6c23e37b166aad3bbd0574
jfmtucci <jfmtucci@gmail.com>
2024-06-23 19:18:40 -0400

    subiendo proyecto

---
+++ assets/js/listadoPropiedadesVenta.js
@@ -0,0 +1,87 @@
+const propiedadVenta = [
+  {
+    nombre: "Apartamento de lujo en zona exclusiva",
+    src: "https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg",
+    descripcion:
+      "Este apartamento de lujo está ubicado en una exclusiva zona residencial",
+    ubicacion: "123 Luxury Lane, Prestige Suburb, CA 45678",
+    habitaciones: "4",
+    baños: "4",
+    costo: "5.000",
+    smoke: false,
+    pets: false,
+  },
+  {
+    nombre: "Apartamento acogedor en la montaña",
+    src: "https://cdn.bioguia.com/embed/3d0fb0142790e6b90664042cbafcb1581427139/furgoneta.jpg",
+    descripcion:
+      "Este apartamento acogedor está situado en lo alto de una montaña con impresionantes vistas",
+    ubicacion: "789 Mountain Road, Summit Peaks, CA 23456",
+    habitaciones: "2",
+    baños: "1",
+    costo: "1.200",
+    smoke: true,
+    pets: true,
+  },
+  {
+    nombre: "Penthouse de lujo con terraza panorámica",
+    src: "https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg",
+    descripcion:
+      "Este penthouse de lujo ofrece una terraza panorámica con vistas espectaculares",
+    ubicacion: "567 Skyline Avenue, Skyview City, CA 56789",
+    habitaciones: "3",
+    baños: "3",
+    costo: "4.500",
+    smoke: false,
+    pets: true,
+  },
+  {
+    nombre: "Apartamento de lujo en zona exclusiva",
+    src: "./assets/img/casaVenta4.jpg",
+    descripcion:
+      "Este apartamento de lujo está ubicado en una exclusiva zona residencial",
+    ubicacion: "123 Luxury Lane, Prestige Suburb, CA 45678",
+    habitaciones: "4",
+    baños: "4",
+    costo: "5.000",
+    smoke: false,
+    pets: false,
+  },
+  {
+    nombre: "Apartamento acogedor en la montaña",
+    src: "./assets/img/casaVenta5.jpg",
+    descripcion:
+      "Este apartamento acogedor está situado en lo alto de una montaña con impresionantes vistas",
+    ubicacion: "789 Mountain Road, Summit Peaks, CA 23456",
+    habitaciones: "2",
+    baños: "1",
+    costo: "1.200",
+    smoke: true,
+    pets: true,
+  },
+  {
+    nombre: "Penthouse de lujo con terraza panorámica",
+    src: "./assets/img/casaVenta6.jpg",
+    descripcion:
+      "Este penthouse de lujo ofrece una terraza panorámica con vistas espectaculares",
+    ubicacion: "567 Skyline Avenue, Skyview City, CA 56789",
+    habitaciones: "3",
+    baños: "3",
+    costo: "4.500",
+    smoke: false,
+    pets: true,
+  },
+  {
+    nombre: "Penthouse de lujo con terraza panorámica",
+    src: "./assets/img/casa7.jpg",
+    descripcion:
+      "Este penthouse de lujo ofrece una terraza panorámica con vistas espectaculares",
+    ubicacion: "567 Skyline Avenue, Skyview City, CA 56789",
+    habitaciones: "3",
+    baños: "3",
+    costo: "4.500",
+    smoke: false,
+    pets: true,
+  },
+];
+export default propiedadVenta;
---
+++ assets/js/propiedades_alquiler.js
@@ -0,0 +1,60 @@
+import propiedadesAlquiler from "./listadoPropiedadesAlquiler.js";
+
+const mostrarAlquiler = () => {
+  const container = document.getElementById("alquiler");
+
+  propiedadesAlquiler.forEach((propiedad) => {
+    const card = document.createElement("div");
+    card.className = "col-md-4 mb-4";
+    card.innerHTML = ` <div class="card">
+              <img
+                src="${propiedad.src}"
+                class="card-img-top"
+                alt="Imagen del departamento"
+              />
+              <div class="card-body">
+                <h5 class="card-title">
+                  ${propiedad.nombre}
+                </h5>
+                <p class="card-text">
+                  ${propiedad.descripcion}
+                </p>
+                <p>
+                  <i class="fas fa-map-marker-alt"></i> ${propiedad.ubicacion}
+                </p>
+                <p>
+                  <i class="fas fa-bed"></i> ${
+                    propiedad.habitaciones
+                  } Habitaciones
+                  <i class="fas fa-bath"></i> ${propiedad.baños} Baños
+                </p>
+                <p><i class="fas fa-dollar-sign"></i>${propiedad.costo}</p>
+                <p class=${
+                  propiedad.smoke === true ? "text-success" : "text-danger"
+                } >
+                  <i class="${
+                    propiedad.smoke === true
+                      ? "fas fa-smoking"
+                      : "fas fa-smoking-ban"
+                  }"></i> ${
+      propiedad.smoke === true ? " Permitido fumar" : " No se permite fumar"
+    }
+                </p>
+                <p class=${
+                  propiedad.pets === true ? "text-success" : "text-danger"
+                }>
+                  <i class="${
+                    propiedad.pets === true ? "fas fa-paw" : "fa-solid fa-ban"
+                  }"></i> ${
+      propiedad.pets === true
+        ? " Mascotas permitidas"
+        : " No se permiten mascotas"
+    }
+                </p>
+              </div>
+            </div>`;
+    container.appendChild(card);
+  });
+};
+
+mostrarAlquiler();
